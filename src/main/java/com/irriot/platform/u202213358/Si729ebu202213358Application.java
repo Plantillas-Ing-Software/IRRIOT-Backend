f0ec8bf5dd355402a7018dca773523c09cf7e13d
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-19 01:52:44 -0500

    chore: initial commit

---
+++ src/main/java/com/irriot/platform/u202213358/Si729ebu202213358Application.java
@@ -0,0 +1,13 @@
+package com.irriot.platform.u202213358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729ebu202213358Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Si729ebu202213358Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/irriot/platform/u202213358/Si729ebu202213358ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irriot.platform.u202213358;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729ebu202213358ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
